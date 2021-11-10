837b6652f39671b40ebba320c002ccca1b844995
Nithin R K <87193277+spArTans5@users.noreply.github.com>
2021-11-10 21:40:27 +0530

    Update test_bench.v

--- Traffic light controller/test_bench.v
+++ Traffic light controller/test_bench.v
@@ -1,1 +1,19 @@
+`timescale 1ms/1us
 
+`include "TLC.v"
+
+module TLC_tb;
+reg clk,rst;
+wire [2:0]yns,yew;
+TLC_1 dut(.rst(rst),.clk(clk),.yNS(yns),.yEW(yew));
+initial 
+begin
+	   rst<=1'b1;
+	#1000 rst<=1'b0;
+end
+initial 
+begin
+	clk<=1'b1;
+	forever #500 clk<=~clk;
+end
+endmodule
